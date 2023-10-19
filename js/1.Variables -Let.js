292bb2c8e0fdeceede259757c03f93425afc049e
MemersonSt <emerson.cajape@tecsu.edu.ec>
2023-10-19 09:15:52 -0500

    Clases de Javascript

---
+++ js/1.Variables -Let.js
@@ -0,0 +1,17 @@
+// Variables - let 
+let cliente = "Juan"
+let precioDescuento = 220 // camelCase
+
+// Re asignar
+cliente = 20
+cliente = true
+
+// Pueden iniciar sin un valor
+let precio
+
+precio = 1000
+
+console.log(precio) //the result is 1000 because the last value is 1000
+
+console.log(cliente) //the result is true because the last value is true
+console.log(precioDescuento)
---
+++ js/2.Variables-Const.js
@@ -0,0 +1,15 @@
+// Variables - const
+const cliente = "Juan"
+const precioDescuento = 220 
+
+// No se puede re asignar
+cliente = "Pablo"
+
+// No pueden iniciar sin valor
+// const precio
+
+
+
+console.log(precio) //the result is undefined
+console.log(cliente) //the result is Juan
+console.log(precioDescuento) //the result is 220
